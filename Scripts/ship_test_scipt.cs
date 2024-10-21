be5838dcf8015e175179ac3ef589d6a6fb2eb033
Joas160 <161321201+Joas160@users.noreply.github.com>
2024-10-21 16:15:20 +0200

    ship can be moved

--- Scripts/ship_test_scipt.cs
+++ Scripts/ship_test_scipt.cs
@@ -1,11 +1,36 @@
+using System;
+using System.Threading.Tasks;
 using Godot;
-//using System;
-//using System.Reflection.Metadata;
-public partial class ship_test_scipt : Area2D{
-	public override void _PhysicsProcess(double delta)
+using Godot.NativeInterop;
+
+public partial class ship_test_scipt : Node2D{
+	public override void _Process(double delta)
 	{
-		if(((bool)GetMeta("Selected"))== true && Input.IsActionJustPressed("rotate_selected_ship")){
-			RotationDegrees += 90;
+		if(((bool)GetMeta("Selected"))== true ){
+			//follows mouse
+			Position = GetGlobalMousePosition();
+			//rotation
+			if( Input.IsActionJustPressed("rotate_selected_ship")){
+				RotationDegrees += 90;
+			}
 		}
+		//selects current ship
+		if((bool)GetMeta("Hovered") == true && Input.IsActionJustPressed("select")){
+			if((bool)GetMeta("Selected") == true){
+				SetMeta("Selected", false);
+			}
+			else{
+				SetMeta("Selected", true);
+			}
+		}
+	}
+	//checks if the ship is hovered over
+	public void _on_area_2d_mouse_entered(){
+		SetMeta("Hovered", true);
+		GetNode<Area2D>("Area2D").GetNode<AnimatedSprite2D>("AnimatedSprite2D").Scale = new Vector2(1.1F,1.1F);
+	}
+	public void _on_area_2d_mouse_exited(){
+		SetMeta("Hovered", false);
+		GetNode<Area2D>("Area2D").GetNode<AnimatedSprite2D>("AnimatedSprite2D").Scale = new Vector2(1.0F,1.0F);
 	}
 }
