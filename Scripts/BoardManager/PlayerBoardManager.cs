2e7991a1f8aa76fdcb199197a3ac278e59774c56
EliasHab <eliashab20@gmail.com>
2025-01-22 15:31:38 +0100

    GUI von Arwed angeschaut

---
+++ Scripts/BoardManager/PlayerBoardManager.cs
@@ -0,0 +1,20 @@
+namespace BattleSchiffe.Scripts.BoardManager;
+using System;
+using System.Collections.Generic;
+using Godot;
+
+public partial class PlayerBoardManager : BoardManager
+{
+	/*public void SetShip(List<int[,]> positions, ShipManager shipManager)
+	{
+		Ship newShip = new Ship();
+		newShip.shipPosition = positions;
+		//newShip.shipManager = shipManager;
+		ships.Add(newShip);
+	}*/
+
+	public int GetShipCount()
+	{
+		return ships.Count;
+	}
+}
